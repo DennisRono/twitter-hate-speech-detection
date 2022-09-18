256324d3c796945f947f6e44793bbe123b26139a
DennisRono <finnneron1@gmail.com>
2022-09-18 21:06:02 +0300

    setting piece stick two hollow

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import Header from '../components/Header'
 import { Helmet } from "react-helmet"
 import Footer from '../components/Footer'
+import Breadcrumb from '../components/Breadcrumb'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
             <title>Home | Twitter Hate Speech Detection</title>
         </Helmet>
         <Header/>
+        <Breadcrumb/>
         <Footer/>
     </Fragment>
   )
