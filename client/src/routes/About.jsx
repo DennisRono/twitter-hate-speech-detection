11818b1f77d2ef8f8985f85ff514c96b109722b5
DennisRono <finnneron1@gmail.com>
2022-09-19 13:50:01 +0300

    excellent summer sharp pony pleasure

---
+++ client/src/routes/About.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+        <div className="about">
+            <h1 className="about-title">Twitter hate crime and cyberbullying detection</h1>
+        </div>
+    </Fragment>
+  )
+}
+
+export default About
