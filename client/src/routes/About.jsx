613eec0e80a6d855eca5b653234e11808b18205e
DennisRono <finnneron1@gmail.com>
2022-09-19 14:10:01 +0300

    zipper frame anything habit telephone

--- client/src/routes/About.jsx
+++ client/src/routes/About.jsx
@@ -14,7 +14,9 @@ const About = () => {
         <Header/>
         <div className="about">
             <h1 className="about-title">Twitter hate crime and cyberbullying detection</h1>
-            <p className="creator">Created by <a href="https://github.com/zarina">Cynthia Chelang'at <Send className="sendicon"/></a></p>
+            <p className="creator">Created by <a href="https://github.com/zarina" rel="noreferrer" target="_blank">Cynthia Chelang'at <Send className="sendicon"/></a></p>
+            <p className="abt-cont">This project aims to <strong>automate content moderation</strong> to identify hate speech using <strong>machine learning binary classification algorithms.</strong></p>
+            <p className="abt-cont">Baseline models included Random Forest, Naive Bayes, Logistic Regression and Support Vector Machine (SVM). The final model was a <strong>Logistic Regression</strong> model that used Count Vectorization for feature engineering. It produced an F1 of 0.3958 and Recall (TPR) of 0.624.</p>
         </div>
     </Fragment>
   )
